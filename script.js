6eff4421a5cf1c2974e49615c05a4a7587dce1b4
borgesarturr <artur.efivest.10@gmail.com>
2026-05-28 22:24:29 -0300

    atualização 2.0

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let produtos = [
   { produto: "camisa preta", preco: 99.99 },
   { produto: "bone preto", preco: 159.90 },
-  { produto: "moleton", preco: 199 },
-  { produto: "jaqueta couro", preco: 399 }
+  { produto: "moleton", preco: 199.00 },
+  { produto: "jaqueta couro", preco: 399.00 }
 ];
 
 let carrinho = [];
@@ -11,7 +11,7 @@ function adicionarProduto(nomeProduto) {
   // O .find() vai buscar na lista de produtos o item que tem o mesmo nome
   let itenEncontrado = produtos.find(item => item.produto === nomeProduto);
 
-   if (itenEncontrado) {
+  if (itenEncontrado) {
     carrinho.push(itenEncontrado);
     console.log(`${nomeProduto} foi adicionado ao carrinho!`);
   } else {
@@ -21,16 +21,22 @@ function adicionarProduto(nomeProduto) {
 }
 
 function calcularTotal() {
-// Lembre-se, quando um loop for trabalhar com total, sempre começará em 0;
+  // Lembre-se, quando um loop for trabalhar com total, sempre começará em 0;
+
+  let mostrarTotal = document.getElementById('texto-total').innerText
+
   let total = 0;
   // 
-    for (let item of carrinho) {
-      // Soma o preço do item atual ao total acumulado
-      total += item.preco;
-    }
+  for (let item of carrinho) {
+    // Soma o preço do item atual ao total acumulado
+    total += item.preco;
+  }
 
-    console.log(`O valor total do carrinho é: R$ ${total.toFixed(2)}`);
-  // O .toFixed(2) serve só para deixar o preço bonitinho com dois centavos (ex: 99.99)
+  const elementoTotal = document.getElementById('texto-total');
+  elementoTotal.innerText = `O valor total do carrinho é: R$ ${total.toFixed(2)}`;
+
+
+
 }
 
 adicionarProduto("camisa preta")
@@ -46,9 +52,27 @@ const botoes = document.querySelectorAll('.btn-adicionar');
 
 // Para CADA botao que existe dentro dessa lista de botoes...
 botoes.forEach(botao => {
-    
-    // Aqui dentro nós vamos escutar o clique de cada um deles!
-    let botoes = 
-    
+
+  // Aqui dentro nós vamos escutar o clique de cada um deles!
+  botao.addEventListener('click', function () {
+    // Captura o nome do produto do botão clicado
+    let produtoClicado = botao.dataset.produto;
+
+    // Chama a função que criamos no primeiro dia passando esse nome!
+    adicionarProduto(produtoClicado);
+
+    calcularTotal()
+
+  })
+
+
 });
+
+document.body.style.backgroundColor = "#106EBE";
+document.body.style.textAlign = "center";
+
+
+
+
+
 
